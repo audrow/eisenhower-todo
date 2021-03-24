2b225e137b74c8e7875013b910ee996286c308d7
Audrow Nash <audrow.nash@gmail.com>
2021-03-23 22:10:24 -0700

    Add basic CRUD (#2)
    * Create basic todo list with create function
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Refactor file structure
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Add basic CRUD operations
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>

---
+++ app.ts
@@ -0,0 +1,17 @@
+import { Application } from "https://deno.land/x/oak/mod.ts"
+import router from "./routes/todo-routes.ts"
+
+const app = new Application()
+
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+await app.listen({ port: 8000 })
