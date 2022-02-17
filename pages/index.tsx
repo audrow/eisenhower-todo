c0e6cf827304267bc1709245dc50f822d724b139
Audrow Nash <audrow@hey.com>
2022-02-17 07:10:17 -0800

    Add cypress and e2e tests
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <nav>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
+  )
 }
